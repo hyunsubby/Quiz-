78de67bd7bed12ef08e3fbcbfb28f74d73956580
hyunsubby <95265154+hyunsubby@users.noreply.github.com>
2022-02-04 13:53:11 +1000

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,59 @@
+
+let questions = [
+    {
+    numb: 1,
+    question: "Which is the third planet from the sun?",
+    answer: "Earth",
+    options: [
+      "Saturn",
+      "Earth",
+      "Pluto",
+      "Mars"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is the largest ocean on Earth?",
+    answer: "Pacific Ocean",
+    options: [
+      "Atlantic Ocean",
+      "Indian Ocean",
+      "Arctic Ocean",
+      "Pacific Ocean"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What is the capital of Australia?",
+    answer: "Canberra",
+    options: [
+      "Sydney",
+      "Canberra",
+      "Melbourne",
+      "Perth"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What was the name of the ship which rescued survivors from the Titanic in 1912?",
+    answer: "Carpathia",
+    options: [
+      "Carpathia",
+      "Mayflower",
+      "Advenna Avis",
+      "Sweet Madonna"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the capital of South Korea",
+    answer: "Seoul",
+    options: [
+      "Seoul",
+      "Busan",
+      "Jeju",
+      "Bucheon"
+    ]
+  },
+
+];
